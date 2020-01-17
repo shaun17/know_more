a26971310c11e1f69226b5b250e5dd71c639a0ad
shaun <shaun.17@outlook.com>
2020-01-17 22:59:14 +0800

    aa

--- src/main/java/map/hashmap/Demo.java
+++ src/main/java/map/hashmap/Demo.java
@@ -1,7 +1,24 @@
 package map.hashmap;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.util.comparator.ComparableComparator;
+
 public class Demo {
 	public static void main(String[] args) {
-		 System.out.println(2^3);
+		List<String> list = Arrays.asList("1","22","333","4444","55555");
+//		Collections.sort(list, new Comparator<String>() {
+//			@Override
+//			public int compare(String o1, String o2) {
+//				return o2.length()-o1.length();
+//			}
+//		});
+		Collections.sort(list, (s1,s2)->(s1.length()-s2.length()));
+		System.out.println(list);
 	}
 }
